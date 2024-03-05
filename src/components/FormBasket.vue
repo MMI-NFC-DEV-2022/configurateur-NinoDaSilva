50a2e18525b8e114aa001f78b9d2138718b7c8ee
Nino DA SILVA <ninoportos@gmail.com>
2024-03-05 16:08:54 +0100

    Base passage de données à un formulaire

---
+++ src/components/FormBasket.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import type { Basket } from '@/types'
+import SvgProfil from '@/components/SvgProfil.vue'
+import SvgDessus from '@/components/SvgDessus.vue'
+
+const props = defineProps<{
+  data?: Basket;
+  id?: string;
+}>();
+
+const basket = ref<Basket>(props.data ?? {});
+</script>
+
+<template>
+  <div class="p-2">
+    <ul class="flex gap-1">
+      <li><a href="#profil">Profil</a></li>
+      <li><a href="#dessus">Dessus</a></li>
+    </ul>
+    <div class="carousel w-64">
+      <SvgProfil class="carousel-item w-64" v-bind="basket" id="profil"></SvgProfil>
+      <SvgDessus class="carousel-item w-64" v-bind="basket" id="dessus"></SvgDessus>
+    </div>
+
+    <FormKit type="form" v-model="basket">
+      <FormKit name="semelle" label="semelle" value="#ffffff" type="color"/>
+      <FormKit name="empeigne" label="empeigne" value="#ffffff" type="color"/>
+      <FormKit name="pointe" label="pointe" value="#ffffff" type="color"/>
+      <FormKit name="oeillet" label="oeillet" value="#ffffff" type="color"/>
+      <FormKit name="bande" label="bande" value="#ffffff" type="color"/>
+      <FormKit name="languette" label="languette" value="#ffffff" type="color"/>
+      <FormKit name="lacet" label="lacet" value="#ffffff" type="color"/>
+      <FormKit name="trimestre" label="trimestre" value="#ffffff" type="color"/>
+    </FormKit>
+  </div>
+</template>
